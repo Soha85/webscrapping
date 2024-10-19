812cb02668828f7f40de66e7df40a6c0912352c6
Soha <sohaelsafey@yahoo.com>
2024-10-19 18:17:43 +0300

    add evaluation for retrieval and generation

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ def rag_generate(query,context,temperature):
     try:
         articles_llm = pipeline('text-generation', model='gpt2', batch_size=128)
         #llm.model.config.pad_token_id = llm.model.config.eos_token_id
+        print(query,context)
         generated = articles_llm(f"Query: {query}\nContext: {context}\nAnswer:",max_new_tokens=200,temperature=temperature,num_return_sequences=1)
         return generated[0]['generated_text'].split('Answer:')[1]
 
