c985d8977f23d2d62f4335f75abee65282ddcde4
Soha <sohaelsafey@yahoo.com>
2024-10-19 14:04:57 +0300

    Faiss and Cosine Similarities retrieval edits

--- RAG.py
+++ RAG.py
@@ -60,7 +60,7 @@ class RAG:
 
     # Chunking: Split long documents into smaller chunks
     def chunk_text(self,text, chunk_size, overlap):
-        words = text.split()
+        words = text.split(' ')
         chunks = [' '.join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size - overlap)]
         return chunks
 
