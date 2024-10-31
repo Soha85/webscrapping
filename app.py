4ebf298c5a3f95670a7e365ec48b413f874ca664
Soha <sohaelsafey@yahoo.com>
2024-10-31 19:33:10 +0300

    split page into two columns

--- app.py
+++ app.py
@@ -99,10 +99,10 @@ with col1:
             if articles:
                 # Display articles in a table
                 RAG.articles = pd.DataFrame({'title': titles, 'content': articles})
-                st.write("**Processing articles, Start Chunking and Emdeddings...**")
-                # Preparing Data
-                rag_instance = RAG()
-                st.write(rag_instance.prepare_data(chunk_size, overlap))
+            st.write("**Processing articles, Start Chunking and Emdeddings...**")
+            # Preparing Data
+            rag_instance = RAG()
+            st.write(rag_instance.prepare_data(chunk_size, overlap))
 
 
             st.session_state.articles_df = RAG.articles
